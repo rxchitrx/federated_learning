52ec667e3af4e1a26cde660232728fd24f454b9d
rxchitrx <rachitmital@gmail.com>
2026-04-30 18:16:57 +0530

    Added model.py

---
+++ model.py
@@ -0,0 +1,34 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2,2)
+
+
+        self.fc1 = nn.Linear(64*7*7,128)
+        self.fc2 = nn.Linear(128, 2)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 64 * 7 * 7)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+if __name__ == "__main__":
+    import torch
+
+    model = SimpleCNN()
+
+    fake_image = torch.randn(1,1,28,28)
+
+    output = model(fake_image)
+
+    print(f"Model initialized successfully!")
+    print(f"Output shape: {output.shape}")
